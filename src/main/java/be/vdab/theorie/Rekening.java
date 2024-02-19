05a8f3c19a9d579b83bb15d27f0073179f631785
SergioBelgium <137648912+SergioBelgium@users.noreply.github.com>
2024-02-19 15:32:31 +0100

    commit met Collections,arrays

--- src/main/java/be/vdab/theorie/Rekening.java
+++ src/main/java/be/vdab/theorie/Rekening.java
@@ -1,18 +1,28 @@
 package be.vdab.theorie;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Rekening {
+    private final List<BigDecimal> stortingen = new ArrayList<>();
 
-    private BigDecimal saldo=BigDecimal.ZERO;
+   // private BigDecimal saldo=BigDecimal.ZERO;
     public void  stort(BigDecimal bedrag){
         if (bedrag.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Bedrag moet positief zijn");
         }
-        saldo = saldo.add(bedrag);
+        stortingen.add(bedrag);
 
     }
     public BigDecimal getSaldo(){
-       return saldo;
+        return stortingen.stream().reduce(BigDecimal.ZERO,
+                (vorigSaldo, storting) -> vorigSaldo.add(storting));
+    }
+    List<BigDecimal> getStortingen() {
+        return stortingen;
+    }
+    List<BigDecimal> getStortingenGesorteerd() {
+        return stortingen.stream().sorted().toList();
     }
 
 }
