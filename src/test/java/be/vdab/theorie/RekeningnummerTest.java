3378ee0727f40cf49e41fbf256026cb238fe53d2
SergioBelgium <137648912+SergioBelgium@users.noreply.github.com>
2024-02-20 09:49:09 +0100

    commit ParameterizedTest

--- src/test/java/be/vdab/theorie/RekeningnummerTest.java
+++ src/test/java/be/vdab/theorie/RekeningnummerTest.java
@@ -1,11 +1,19 @@
 package be.vdab.theorie;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 class RekeningnummerTest {
-    @Test
+    @ParameterizedTest
+    @ValueSource (strings = {"BE72091012240116", "BE68539007547034",
+            "BE02063588295840"})
+    void correcteNummers(String nummer){//fijate que aki aparecen parametros!!
+        new Rekeningnummer(nummer);
+    }
+   /* @Test                                                    // deze 3 test methodes zijn vervangen door de ParameterizedTest en ValueSource!
     void BE72091012240116IsCorrect() {
         new Rekeningnummer("BE72091012240116");
     }
@@ -16,8 +24,17 @@ class RekeningnummerTest {
     @Test
     void BE02063588295840IsCorrect() {
         new Rekeningnummer("BE02063588295840"); // met eencontrolegetal < 10
-    }
-    @Test
+    }*/
+   @ParameterizedTest                 //Reemplazamos los test metodos que deben generar Exception, por ParameterizedTest!, uno de los inconvenientes es que perdemos info de la descripcion de cada meotdo
+   @ValueSource(strings={ "BE720910122401160", "BE7209101224011",
+           "NL72091012240116", "BEX209101224011", "BE01091012240116",
+           "BE99091012240116", "BE72091012240115", ""})
+   void verkeerdeNummers(String nummer) {
+       assertThatIllegalArgumentException().isThrownBy(
+               () -> new Rekeningnummer(nummer));
+   }
+
+   /* @Test
     void BE720910122401160IsTeLang() {
         assertThatIllegalArgumentException().isThrownBy(() ->
                 new Rekeningnummer("BE720910122401160"));
@@ -60,7 +77,7 @@ class RekeningnummerTest {
     void leegNummerIsVerkeerd() {
         assertThatIllegalArgumentException().isThrownBy(() ->
                 new Rekeningnummer(""));
-    }
+    }*/
     @Test
     void toStringGeeftHetNummerTerug() {
         var nummer = "BE72091012240116";
