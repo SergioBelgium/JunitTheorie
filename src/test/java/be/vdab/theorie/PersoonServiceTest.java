ad5893195ba49993bee23bd7d0a0096cb1abce3d
SergioBelgium <137648912+SergioBelgium@users.noreply.github.com>
2024-02-25 19:00:45 +0100

    commit StandaardAfwijking, Dependency Injection, Mock gemakt!

--- src/test/java/be/vdab/theorie/PersoonServiceTest.java
+++ src/test/java/be/vdab/theorie/PersoonServiceTest.java
@@ -2,20 +2,35 @@ package be.vdab.theorie;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class PersoonServiceTest {
+    @Mock
+    private PersoonRepository repository;
     private PersoonService service;
 
    @BeforeEach
     public void beforeEach() {
-        service = new PersoonService(new PersoonRepositoryStub());   //aki ya vemos la "dependency injection"!!
+        service = new PersoonService(repository);   //aki ya vemos la "dependency injection"!!
 
     }
     @Test
-    public void deWeddeStandaardAfwijkingIsPositief() {
+    public void deWeddeStandaardAfwijkingIs2() {                         //con entrenamiento de MOCK!!
+       when(repository.findAllWeddes()).thenReturn(Stream.of(2, 4, 4, 4, 5, 5, 7, 9)
+               .map(getal -> BigDecimal.valueOf(getal))
+               .toList());
         assertThat(service.standaardAfwijkingWeddes()).isEqualByComparingTo("2");
+        verify(repository).findAllWeddes();
     }
 }
